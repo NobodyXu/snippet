91cf277692d95b8f91bd7e102ec9d6e28053cbe0
NobodyXu <johnxujiahao2000@gmail.com>
2018-07-15 18:32:54 +1000

    Add algorithm/graph/indirected/edge.hpp.

---
+++ src/learning/algorithm/graph/edge.hpp
@@ -0,0 +1,10 @@
+#ifndef __nobodyxu_wheels_edge_HPP__
+# define __nobodyxu_wheels_edge_HPP__
+
+namespace nxwheels {
+template <class Index_t>
+struct edge {
+    Index_t beg, end;
+};
+} /* nxwheels */
+#endif
