edf950b79da0a34e51de441d790b5803e050a25f
NobodyXu <johnxujiahao2000@gmail.com>
2018-07-17 18:02:42 +1000

    Add quick-find_UF.hpp.

---
+++ src/learning/algorithm/dynamic_connectivity/quick-find_UF.hpp
@@ -0,0 +1,24 @@
+#ifndef __nobodyxu_wheels_dynamic_connectivity_quick_find_UF_HPP__
+# define __nobodyxu_wheels_dynamic_connectivity_quick_find_UF_HPP__
+
+# include "UF_api.hpp"
+
+namespace nxwheels::dyn_union {
+template <class Int>
+struct QF_UF: UF_impl1<Int, QF_UF<Int>> {
+    using base = UF_impl1<Int, QF_UF<Int>>;
+    void Union(Int p, Int q) noexcept {
+        auto pid = base::_id[p];
+        auto qid = base::_id[q];
+
+        if (pid == qid)
+            return;
+
+        for (auto &id: base::_id)
+            id = id == pid ? qid : id;
+        --base::count;
+    }
+    void findRoot(Int p) const noexcept { return base::_id[p]; }
+};
+} /* nxwheels::dyn_union */
+#endif
