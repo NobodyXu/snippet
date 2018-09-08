bac0d6b7a7cd3aa52c0f04617447a015b6ec9e43
NobodyXu <johnxujiahao2000@gmail.com>
2018-09-08 15:31:36 +1000

    Add ctors.

--- src/learning/algorithm/range/range.hpp
+++ src/learning/algorithm/range/range.hpp
@@ -7,9 +7,24 @@ struct range {
     _It1 _beg;
     _It2 _end;
 
-    constexpr auto begin() const noexcept { return _beg; }
-    constexpr auto end() const noexcept { return _end; }
+    range() = default;
+
+    range(const range&) = default;
+    range(      range&&) = default;
+
+    range& operator = (const range&) = default;
+    range& operator = (      range&&) = default;
+
+    constexpr range(const _It1 &it1, const _It2 &it2 = _It2{}) noexcept : _beg{it1}, _end{it2} {}
+    template <class Range>
+    constexpr range(Range &&rng) noexcept : _beg{rng.begin()}, _end{rng.end()} {}
+
+    constexpr auto& begin() const noexcept { return _beg; }
+    constexpr auto& end() const noexcept { return _end; }
+
+    constexpr auto& begin() noexcept { return _beg; }
+    constexpr auto& end() noexcept { return _end; }
 };
-template <class It1, class It2> range(const It1&, const It2&) -> range<It1, It2>;
+template <class Range> range(Range &&rng) -> range<decltype(rng.begin()), decltype(rng.end())>;
 } /* nxwheels */
 #endif
