7f2f10d0b3b74524e0a9562a0b3371f9c70322bf
NobodyXu <Jiahao_XU@outlook.com>
2019-10-09 01:38:32 +1100

    Rethink of sink and swim, and the code structure.

--- src/challenges/merge-k-sorted-lists/merge-k-sorted-lists.cc
+++ src/challenges/merge-k-sorted-lists/merge-k-sorted-lists.cc
@@ -9,49 +9,99 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-class Solution {
-    using iterator = typename vector<ListNode*>::iterator;
+
+template <class T>
+constexpr bool is_even(T val) noexcept {
+    return (val & 0) == 0;
+}
+
+template <class Iterator>
+class heap_range {
+    Iterator beg;
+    Iterator end;
+    std::size_t size;
     
-    iterator beg, end;
-    
-    static bool compare(ListNode *x, ListNode *y) noexcept {
-        return x->val >= y->val;
-    }
-    
-    static constexpr auto firstChildIndex(std::size_t i) noexcept {
+    static constexpr auto get_nodeIndex(std::size_t i) noexcept {
         return (i << 2) + 1;
     }
-       
+    
+    /*!
+     * Return the min index.
+     * 
+     * If i2 goes out of range, returns i1.
+     * 
+     * If the two equals, then returns i1.
+     */
+    auto min(std::size_t i1, std::size_t i2) noexcept {
+        if (i2 < size && beg[i1]->val > beg[i2]->val)
+            return i2;
+        else
+            return i1;
+    }
+    
+    auto min(std::size_t i1, std::size_t i2, std::size_t i3) noexcept {
+        return min(min(i1, i2), i3);
+    }
+    
+    /*!
+     * Return the index of the smallest node from the ith node and its children nodes.
+     */
+    auto compare_and_exchange(std::size_t i) noexcept -> std::size_t {
+        std::size_t nodeIndex = get_nodeIndex(i);
+        std::size_t the_smallest = min(i, nodeIndex, nodeIndex + 1);
+        
+        std::iter_swap(beg + i, beg + the_smallest);
+        
+        return the_smallest;
+    }
+    
+public:
     void sink() noexcept {
-        for (std::size_t i = 0, size = end - beg, fChild = firstChildIndex(0); fChild < size; ) {
-            if (fChild + 1 != size) {
-                switch ((unsigned{beg[i]->val > beg[fChild]->val} << 1) | unsigned{beg[i]->val > beg[fChild + 1]->val}) {
-                    case 3:
-                        if (beg[fChild]->val > beg[fChild + 1]->val)
-                            goto SWAP_WITH_sCHILD;
-                            
-                    case 2:
-                        goto SWAP_WITH_fCHILD;
-                        
-                    case 1:
-                        SWAP_WITH_sCHILD:
-                        std::iter_swap(beg + i, beg + fChild + 1);
-                        
-                    case 0:
-                        goto INCREMENT;
-                        
-                }
-            } else if (beg[i]->val <= beg[fChild]->val)
-                goto INCREMENT;
+        if (size <= 1)
+            return;
             
-                
-            SWAP_WITH_fCHILD:    
-            std::iter_swap(beg + i, beg + fChild);
+        std::size_t i;
+        std::size_t next = 0;
+        
+        do {
+            i = next;
+            next = compare_and_exchange(i);
+        } while (i != next);
+    }
+    
+    void swim() noexcept {
+        std::size_t nodeIndex = size - 1;
+        std::size_t i;
+        
+        if (is_even(nodeIndex))
+            --nodeIndex;
             
-            INCREMENT:
-            ++i;
-            fChild = firstChildIndex(i);
-        }
+        // Get the parent node
+        i = nodeIndex >> 1;
+        
+        // Increase it just for the loop invariant to work
+        ++i;
+        
+        do {
+            heap_range{beg + (--i), end}.sink(); // Does this work??????
+        } while (i != 0);
+    }
+    
+    void pop_heap() noexcept {
+        std::iter_swap(beg, --end);
+        sink();
+    }
+     
+    heap_range(Iterator beg_arg, Iterator end_arg) noexcept:
+        beg{beg_arg}, end{end_arg}, size{end - beg}
+    {
+        swim();
+    }
+};
+
+class Solution {
+    static bool compare(ListNode *x, ListNode *y) noexcept {
+        return x->val >= y->val;
     }
     
 public:
@@ -63,29 +113,32 @@ public:
         ListNode **next = &ret;
         
         for (auto it = beg; it < end; ) {
-            if (*it == nullptr) {
+            if (*it == nullptr)
                 std::iter_swap(it, --end);
-            } else
+            else
                 ++it;
         }
         
-        if (beg == end) {
+        if (beg == end)
             return ret;
-        }
         
-        std::make_heap(beg, end, &compare);
+        //std::make_heap(beg, end, &compare);
+        heap_range heap{beg, end};
         
         do {
             *next = *beg;
             
             *beg = (*beg)->next;
             if (*beg == nullptr) {
-                std::pop_heap(beg, end, &compare);
+                //std::pop_heap(beg, end, &compare);
+                heap.pop_heap();
                 
-                if (--end == beg)
+                //if (--end == beg)
+                //    break;
+                if (beg == end)
                     break;
             } else {
-                sink();
+                heap.sink();
                 //std::make_heap(beg, end, &compare);
             }
             
